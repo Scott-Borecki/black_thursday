cd1fd2ff31ae83f27e6d62a43aa80b704338757b
Michael Abbott <michael@mjabbottdesign.com>
2021-06-03 16:35:46 -0700

    Add TransRepo #find_by_id test

--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -1,3 +1,5 @@
+require 'CSV'
+require_relative '../lib/transaction'
 require_relative '../lib/transaction_repository'
 require 'simplecov'
 
@@ -5,18 +7,26 @@ SimpleCov.start
 
 RSpec.describe TransactionRepository do
 
-  describe 'instantiation' do
+  before :each do
+    @tr = TransactionRepository.new('./spec/fixtures/transactions.csv')
+  end
 
-    before :each do
-      @tr = TransactionRepository.new('./spec/fixtures/transactions.csv')
-    end
+  describe 'instantiation' do
 
     it 'exists' do
       expect(@tr).to be_a(TransactionRepository)
     end
 
     it 'has readable attributes' do
-      expect(@tr.all).to eq([])
+      expect(@tr.all.length).to eq(7)
+    end
+  end
+
+  describe 'methods' do
+
+    it 'can return Transaction by id' do
+      expect(@tr.find_by_id(1).invoice_id).to eq(2179)
+      expect(@tr.find_by_id(256)).to eq(nil)
     end
   end
 end
