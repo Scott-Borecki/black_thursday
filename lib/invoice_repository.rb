dfa4a28cb1ee0701e7a869ac0a563c1981e2386c
Elliot Olbright <elliotolbright@gmail.com>
2021-06-03 16:44:47 -0600

    add fixtures and #return_by_id

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,22 +1,36 @@
+require 'CSV'
+require_relative '../lib/invoice'
+require 'time'
+
 class InvoiceRepository
+  attr_reader :all
 
   def initialize(path)
     @all = []
-    # populate_repository(path)
+    populate_repository(path)
   end
 
   def inspect
    "#<#{self.class} #{@invoices.size} rows>"
   end
 
-  def find_all_by_id(id)
-    all.find { |item| id == item.id }
+  def find_by_id(id)
+    all.find { |invoice| id == invoice.id }
   end
 
-  # def find_by_custumer_id(id)
-  #   all.find { |invoice|  }
-  # end
-
+  def populate_repository(path)
+    CSV.foreach(path, headers: true, header_converters: :symbol) do |row|
+      data_hash = {
+        id:           row[:id].to_i,
+        customer_id:  row[:customer_id].to_i,
+        merchant_id:  row[:merchant_id].to_i,
+        status:       row[:status],
+        created_at:   Time.parse(row[:created_at]),
+        updated_at:   Time.parse(row[:updated_at])
+      }
+      @all << Invoice.new(data_hash)
+    end
+  end
 
 
 end
---
+++ spec/invoice_repository_spec.rb
@@ -0,0 +1,34 @@
+require 'CSV'
+require_relative '../lib/invoice_repository'
+require_relative '../lib/invoice'
+require 'rspec'
+require 'simplecov'
+
+SimpleCov.start
+
+RSpec.describe InvoiceRepository do
+
+  before :each do
+    @i = InvoiceRepository.new('./spec/fixtures/invoices.csv')
+  end
+
+  describe 'Object Creation' do
+
+    it 'exists' do 
+      expect(@i).to be_a(InvoiceRepository)
+    end 
+
+    it 'has readable attributes' do 
+      expect(@i.all).to be_a(Array)
+      expect(@i.all.count).to eq(19)
+    end 
+  end 
+
+  describe 'Object Methods' do
+    
+    it 'can return Invoice by ID' do 
+      expect(@i.find_by_id(1).merchant_id).to eq(12335938)
+    end 
+  end 
+
+end
