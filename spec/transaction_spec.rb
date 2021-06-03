4235bfdb7942716bbda5e5d75dd70fe87cfe3374
Michael Abbott <michael@mjabbottdesign.com>
2021-06-03 15:40:03 -0700

    Add Transaction #initialize code

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe Transaction do
     @t = Transaction.new(
       id:                           6,
       invoice_id:                   8,
-      credit_card_number:           '4242424242424242',
-      credit_card_expiration_date:  '0220',
+      credit_card_number:           '4_242_424_242_424_242',
+      credit_card_expiration_date:  '0_220',
       result:                       'success',
       created_at:                   Time.now,
       updated_at:                   Time.now
@@ -25,11 +25,10 @@ RSpec.describe Transaction do
   it 'has readable attributes' do
     expect(@t.id).to eq(6)
     expect(@t.invoice_id).to eq(8)
-    expect(@t.credit_card_number).to eq('4242424242424242')
-    expect(@t.credit_card_expiration_date).to eq('0220')
+    expect(@t.credit_card_number).to eq('4_242_424_242_424_242')
+    expect(@t.credit_card_expiration_date).to eq('0_220')
     expect(@t.result).to eq('success')
-    expect(@t.created_at).to eq(Time.now)
-    expect(@t.updated_at).to eq(Time.now)
-    expect(@t.all).to eq([])
+    expect(@t.created_at).to be_a(Time)
+    expect(@t.updated_at).to be_a(Time)
   end
 end
