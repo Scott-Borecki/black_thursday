347065da457d8ab5dc086a013fd405d9a64475ae
Scott-Borecki <scottborecki@gmail.com>
2021-06-05 17:08:03 -0600

    Add #merchants_with_only_one_item

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -33,4 +33,36 @@ class SalesAnalyst
     end
     std_dev(num_items_per_merchant)
   end
+
+  # def merchants_with_pending_invoices
+  #   # find all invoices with :pending status
+  #     #=> return array of invoices
+  #   # map over invoice array to return merchant id
+  #     #=> return array of merchant IDs
+  #   # uniq to delete duplicate merchant ids
+  #     #=>return array of unique merchant ids
+  #   # use merchants.find_by_id(merchant_id) to find each merchant object and reduce into an empty array
+  #   #
+  #   # merchant_ids = @sales_engine.invoices.all.map do |invoice|
+  #   #   find_all_by_status(:pending).merchant_id
+  #   # end.uniq
+  #
+  #   # merchant_ids = @sales_engine.invoices.find_all_by_status(:pending).map do |invoice|
+  #   #   invoice.merchant_id
+  #   # end
+  #   # require "pry"; binding.pry
+  #   #
+  #   # merchant_ids.uniq
+  #   # #
+  #   # merchants = merchant_ids.map do |merchant_id|
+  #   #   @sales_engine.merchants.find_by_id(merchant_id)
+  #   # end
+  # end
+
+  def merchants_with_only_one_item
+    @sales_engine.merchants.all.reduce([]) do |array, merchant|
+      array << merchant if @sales_engine.items.find_all_by_merchant_id(merchant.id).count == 1
+      array
+    end
+  end
 end
