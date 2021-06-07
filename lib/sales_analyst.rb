a39e9a0252d1a13623602a753ed40d0e1e99f58d
Michael Abbott <michael@mjabbottdesign.com>
2021-06-07 15:02:10 -0700

    Add #total_revenue_by_date

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -53,4 +53,20 @@ class SalesAnalyst
     end.sum
     big_decimal_total
   end
+
+  def total_revenue_by_date(date)
+    invoices_by_date = @sales_engine.invoices.find_all_by_date(date)
+
+    transactions_by_invoice_id = invoices_by_date.map do |invoice|
+      @sales_engine.transactions.find_all_by_invoice_id(invoice.id)
+    end
+
+    successful_transaction_invoice_ids = transactions_by_invoice_id.flatten.reduce([]) do |results, transaction|
+      results << @sales_engine.invoice_items.find_all_by_invoice_id(transaction.invoice_id) if transaction.result == :success
+    end.flatten.uniq
+
+    successful_transaction_invoice_ids.reduce(0) do |sum, invoice_item|
+      sum += invoice_item.unit_price * invoice_item.quantity
+    end
+  end
 end
