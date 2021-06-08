e8555c5c1a25887e9797c0f02c5d766f6dac8865
Ozzie <mikeosmonson@gmail.com>
2021-06-08 16:49:11 -0600

    Remove comments

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -170,66 +170,7 @@ class SalesAnalyst
     end
   end
 
-  def merchants_with_only_one_item_registered_in_month(month)
-    # convert month input to first three letters
-    month_abb = month[0..2]
-    month_integer = Time.new(2021, month_abb).month
-
-    # merchants.all.find_all do |merchant|
-    #   count = invoices.find_all_by_merchant_id(merchant.id).count do |invoice|
-    #     invoice.created_at.month == month_integer
-    #   end
-    #   merchant.created_at.month == month_integer &&
-    #     count == 1
-    # end
-
-    # invoice_items.all.reduce([]) do |array, invoice_item|
-    #   array << merchants.find_by_id(invoices.find_by_id(invoice_item.invoice_id).merchant_id) if
-    #     merchants.find_by_id(invoices.find_by_id(invoice_item.invoice_id).merchant_id).created_at.month == month_integer &&
-    #       invoices.find_by_id(invoice_item.invoice_id).created_at.month == month_integer &&
-    #         invoice_items.find_all_by_invoice_id(invoice_item.invoice_id).count == 1
-    #   array
-    # end     #=> Array with 10 elements
-
-    # TRY ONLY ONE INVOICE IN GIVEN MONTH
-    # invoices.all.reduce([]) do |array, invoice|
-    #   array << merchants.find_by_id(invoice.merchant_id) if
-    #     invoice.created_at.month == month_integer &&
-    #       invoices.all.find_all do |invoice2|
-    #         month_integer == invoice.created_at.month &&
-    #           invoice.merchant_id == invoice2.merchant_id
-    #       end.count == 1
-    #   array
-    # end     #=> Array with 0 elements
-
-    # TRIED TO TAKE OUT REQUIREMENT OF MERCHANT CREATED DATE TO MATCH MONTH
-    # invoice_items.all.reduce([]) do |array, invoice_item|
-    #   array << merchants.find_by_id(invoices.find_by_id(invoice_item.invoice_id).merchant_id) if
-    #     invoices.find_by_id(invoice_item.invoice_id).created_at.month == month_integer &&
-    #       invoice_items.find_all_by_invoice_id(invoice_item.invoice_id).count == 1
-    #   array
-    # end   #=> Array with 60 elements
-
-    # WRONG
-    # items.all.reduce([]) do |array, item|
-    #   array << merchants.find_by_id(item.merchant_id) if
-    #     merchants.find_by_id(item.merchant_id).created_at.month == month_integer &&
-    #       item.created_at.month == month_integer &&
-    #         items.find_all_by_merchant_id(item.merchant_id).count == 1
-    #   array
-    # end
-  end
-
   def revenue_by_merchant(merchant_id)
-    # invoices.find_all_by_merchant_id(merchant_id).reduce(0) do |sum, invoice|
-    #   if successful_transaction?(invoice.id)
-    #     sum = invoice_items.find_all_by_invoice_id(invoice.id).sum do |invoice_item|
-    #       invoice_item.unit_price * invoice_item.quantity
-    #     end
-    #     sum
-    #   end
-    # end
-
     invoices.find_all_by_merchant_id(merchant_id).sum do |invoice|
       if successful_transaction?(invoice.id) == false
         0
